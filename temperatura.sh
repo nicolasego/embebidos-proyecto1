f161d42be07a7cddca059c288b276285d8c9d16b
nicolasego <80408776+nicolasego@users.noreply.github.com>
2021-03-26 10:27:03 -0500

    Update temperatura.sh

--- temperatura.sh
+++ temperatura.sh
@@ -6,11 +6,11 @@ cd /sys/bus/w1/devices/28-3c01d075651a
 #se genera el bucle para la extracción de la medida de temperatura y el envio al csv
 while [ 1 ]
 do		
-	#se le asiga a la variable temp el valor extraido del sensor
+	#se le asigna a la variable temp el valor extraido del sensor
 	temp=$(cat temperature)
-	#se realiza el adecuamiento de este valor mediante la division por 1000 y se establece el numero de decimales
+	#se realiza el adecuamiento de este valor mediante la división por 1000 y se establece el numero de decimales
 	temp=$(echo "scale=2; $temp/1000" | bc -l)"°C"
-	#se agraga al archivo las dos columas requeridas separadas por coma. las columas son la fecha y la temperatura
+	#se agrega al archivo las dos columnas requeridas separadas por coma. las columnas son la fecha y la temperatura
 	date +"%Y%m%d %H%M%S"","$temp>>/home/pi/$fecha 
 	#se genera el sleep que permite la toma cada 10 segundos
 	sleep 10
